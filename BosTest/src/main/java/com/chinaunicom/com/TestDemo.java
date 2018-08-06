7b9723866aa871ad8d64e014fc170f07efd3e664
songzc <605512258@qq.com>
2018-08-06 22:36:40 +0800

    测试文件

---
+++ BosTest/src/main/java/com/chinaunicom/com/TestDemo.java
@@ -0,0 +1,13 @@
+package com.chinaunicom.com;
+/** 
+* @author  songzch 
+* @date 创建时间：2018年8月6日 下午10:32:50  
+* @parameter  
+* @return  
+*/
+public class TestDemo {
+	public static void main(String[] args) {
+		System.out.println("我是一名努力的程序员！");
+	}
+
+}
